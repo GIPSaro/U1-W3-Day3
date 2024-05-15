99737274cc0c5ed670acf28a21e24a92aac4242b
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-05-15 17:20:47 +0200

    commit5

--- assets/JS/ToDoList.js
+++ assets/JS/ToDoList.js
@@ -1,20 +1,22 @@
-const form = document.getElementsByTagName("form")[0];
+const form = document.getElementsByTagName("form")[0]; //seleziono il primo elemento <form> trovato nel documento HTML e lo memorizzo nella variabile form
 
 form.onsubmit = function (e) {
-  e.preventDefault();
+  //assegno una funzione all'evento submit del form. La funzione viene eseguita ogni volta che il form viene sottoposto
+  e.preventDefault(); //prevengo il comportamento predefinito del form quando viene sottoposto, che è quello di ricaricare la pagina
 
-  const taskInput = document.getElementById("itemTextBox");
+  const taskInput = document.getElementById("itemTextBox"); //seleziono l'elemento dell'input del form con l'id "itemTextBox" e lo memorizzo nella variabile taskInput
   const task = taskInput.value;
 
-  const main = document.getElementsByTagName("main")[0];
+  const main = document.getElementsByTagName("main")[0]; //seleziono il primo elemento <main> nel documento HTML e lo memorizzo nella variabile main
 
   const listItem = document.createElement("p");
   listItem.className = "task";
-  listItem.innerText = task;
+  listItem.innerText = task; // il testo del compito inserito viene impostato come testo dell'elemento <p>.
 
-  const button = document.createElement("button");
+  const button = document.createElement("button"); //creo un nuovo bottone
   button.innerText = "Remove";
   button.onclick = function (e) {
+    //al click del bottone verrà eliminato l'elemento task creato
     const removeBtn = e.target;
     removeBtn.parentNode.remove();
   };
